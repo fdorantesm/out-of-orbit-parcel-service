71a0093dd08723c99608a6bb0ab6b25f22fce2e6
Fernando Dorantes <fdorantesm@gmail.com>
2023-02-22 13:58:30 -0600

    refactor: fix deepsource recommendations

--- libs/common/src/types/storage/storage.type.ts
+++
@@ -1,6 +0,0 @@
-export type StorageConfiguration = {
-  endpoint?: string;
-  publicKey: string;
-  privateKey: string;
-  repository: string;
-};
--- src/core/domain/json.ts
+++
@@ -1,3 +0,0 @@
-export type Json = {
-  [key: string]: any;
-};
