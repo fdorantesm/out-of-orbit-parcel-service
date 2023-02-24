ae92bf77766c8bb2209b34cd2fa96c6b535ac980
Fernando Dorantes <fdorantesm@gmail.com>
2023-02-24 16:54:54 -0600

    refactor: add insertMany support in shipment bulk

--- src/modules/shipments/domain/entities/shipment.entity.ts
+++ src/modules/shipments/domain/entities/shipment.entity.ts
@@ -1,8 +1,14 @@
 import { PackageSize } from '../enums/size.enum';
 import { ShipmentStatus } from '../enums/status.enum';
 import { ShipmentAddress } from '../interfaces/address.interface';
-import { ShipmentPackage } from '../interfaces/package.interface';
-import { Shipment } from '../interfaces/shipment.interface';
+import {
+  CreateShipmentPackage,
+  ShipmentPackage,
+} from '../interfaces/package.interface';
+import {
+  CreateShipmentPayload,
+  Shipment,
+} from '../interfaces/shipment.interface';
 import { DateService } from 'src/modules/shared/services/date.service';
 
 const dateService = new DateService();
@@ -20,12 +26,16 @@ export class ShipmentEntity implements Shipment {
   ) {}
 
   static create(shipment: Shipment): ShipmentEntity {
+    const packet = {
+      ...shipment.packet,
+      size: this.calcSize(shipment.packet.weight),
+    };
     return new ShipmentEntity(
       shipment.uuid,
       shipment.trackingNumber,
       shipment.origin,
       shipment.destination,
-      shipment.packet,
+      packet,
       shipment.userId,
       shipment.status,
       shipment.createdAt,
@@ -76,6 +86,12 @@ export class ShipmentEntity implements Shipment {
 
   public static getInitialStatus() {
     return ShipmentStatus.CREATED;
+  }
+
+  public static isSomePackageNotSupported(
+    shipments: Shipment[] | CreateShipmentPayload[],
+  ): boolean {
+    return shipments.some((shipment) => shipment.packet.weight > 25000);
   }
 
   public toJson() {
